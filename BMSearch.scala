9f5b0ca62cf62e8f40c1194b34a5ae1cc034fd6c
nonoike <ij4nu8d4fw@gmail.com>
2019-01-02 23:47:31 +0900

    バグあり

--- BMSearch.scala
+++ BMSearch.scala
@@ -1,5 +1,29 @@
 object BMSearch extends App {
   val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
-  val pattern = "ドワンゴ".toSeq
+  val reversePattern = "ドワンゴ".toSeq.reverse
+  val skipCountMap = reversePattern.distinct.zipWithIndex.map(e => e._1 -> (if (e._2 == 0) 1 else e._2)).toMap
+  println(s"reversePattern: ${reversePattern}, skipCountMap: ${skipCountMap}")
 
+  private def getSkipCountOrNoneIfMatches(slicedTextChars: Seq[Char]): Option[Int] = {
+    val skipCount = reversePattern.reverse.zip(slicedTextChars)
+      .filter(e => e._1 != e._2)
+      .headOption
+      .map(e => skipCountMap.getOrElse(e._1, reversePattern.length))
+      println(s"slicedTextChars: ${slicedTextChars}, skipCount: ${skipCount}")
+      skipCount
+  }
+
+  private def loop(index: Int, acc: Seq[Int]): Seq[Int] = {
+    val remainTextChars = text.drop(index)
+    println(s"index: ${index}, acc: ${acc}, remainTextChars: ${remainTextChars}")
+
+    if (remainTextChars.length < reversePattern.length) acc
+    else getSkipCountOrNoneIfMatches(remainTextChars.take(reversePattern.length)) match {
+      case Some(i) => loop(index + i, acc)
+      //case None => loop(index + reversePattern.length, index +: acc)
+      case None => loop(index + 1, index +: acc)
+    }
+  }
+
+  loop(0, Nil)
 }
